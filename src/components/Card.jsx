4b15bb0b62e94aa5479e28967b455ad92b564270
KlimovaED <klimovaed@icloud.com>
2024-02-20 13:54:22 +0100

    1

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,28 @@
 import styles from './card.module.scss';
-import React from 'react';
+import React,{useState} from 'react';
 
- function Card(props){
-    const {title,valuta,price,periud,volume,description,tag,active} = props;  
+ function Card({card}){ 
+const[active,setActive]= useState(false);
+
+const handleChange =() =>{
+    setActive(!active);
+}
+
     return(
-        <div className={`${styles.Card} ${styles[tag]}  ${styles[active]}`}>
+        <div key={card.id} style={active 
+                                ? {transform: "scale(1.1)"} 
+                                : {}}
+                                className={`${styles.Card} ${styles[card.color]}`} onClick={handleChange}>
             <div className={styles.card_header}>
-                <h3 className={styles.card-title}>{title}</h3></div>
+                <h3 className="title">{card.title}</h3></div>
             <div className={styles.card_body}>
-                <span className={styles.valuta}>{valuta}</span>
-                <h4 className={styles.price}>{price}</h4>
-                <span className={styles.periud}> / {periud}</span>
+                <span className={styles.valuta}>{card.valuta}</span>
+                <h4 className={styles.price}>{card.price}</h4>
+                <span className={styles.periud}> / {card.periud}</span>
             </div>
             <div className={styles.card_footer}>
-                <p className={styles.volume}> до {volume} Мбит/сек</p>
-                <p className={styles.description}>{description}</p>
+                <p className={styles.volume}> до {card.volume} Мбит/сек</p>
+                <p className={styles.description}>{card.description}</p>
             </div>
         </div>
     )
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,45 +3,22 @@ import Card from "./Card";
 //import styles from './card.module.scss';
 
 function Content() {
+const cards =[ {id:1, color:'blue',title:"Безлимитный 300", valuta:"руб",price:"300",periud:"мес",volume:"10",description:"Объём включенного трафика не ограничен",status:'active'},
+                {id:2,color:'green',title:"Безлимитный 450",valuta:"руб",price:"450",periud:"мес",volume:"50",description:"Объём включенного трафика не ограничен",status:'active'},
+                {id:3,color:'red',title:"Безлимитный 550",valuta:"руб",price:"550",periud:"мес",volume:"100",description:"Объём включенного трафика не ограничен",status:'active'},
+                {id:4,color:'gray',title:"Безлимитный 1000",valuta:"руб",price:"1000",periud:"мес",volume:"200",description:"Объём включенного трафика не ограничен",status:'active'}]
+
+
+
   return (
     <React.Fragment>
-      <Card
-      tag={'blue'}
-        title="Безлимитный 300"
-        valuta="руб"
-        price="300"
-        periud="мес"
-        volume="10"
-        description="Объём включенного трафика не ограничен"
-      />
-      <Card
-      tag={'green'}
-        title="Безлимитный 450"
-        valuta="руб"
-        price="450"
-        periud="мес"
-        volume="50"
-        description="Объём включенного трафика не ограничен"
-      />
-      <Card
-      tag={'red'}
-      active ={'active'}
-        title="Безлимитный 550"
-        valuta="руб"
-        price="550"
-        periud="мес"
-        volume="100"
-        description="Объём включенного трафика не ограничен"
-      />
-      <Card
-      tag={'gray'}
-        title="Безлимитный 1000"
-        valuta="руб"
-        price="1000"
-        periud="мес"
-        volume="200"
-        description="Объём включенного трафика не ограничен"
-      />
+      {
+        cards.map((card)=>{
+          return(
+            <Card card ={card} key={card.id}/>
+          )
+        })
+      }
     </React.Fragment>
   );
 }
